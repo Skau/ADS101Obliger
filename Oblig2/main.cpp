5bbe2d6bdf65197774422165fa3437c88324df64
Skau <kskau93@gmail.com>
2018-10-01 15:00:09 +0200

    Stuff

--- Oblig2/main.cpp
+++ Oblig2/main.cpp
@@ -3,24 +3,64 @@
 
 int main()
 {
-    Node* rot = new Node(5);
+    auto root = new ADS101::Node<int>(5);
 
-    rot->insert(7);
-    rot->insert(9);
-    rot->insert(0);
-    rot->insert(2);
-    rot->insert(3);
-    rot->insert(1);
-    rot->insert(8);
-    rot->insert(4);
-    rot->insert(6);
+    root->insert(7);
+    root->insert(9);
+    root->insert(0);
+    root->insert(2);
+    root->insert(3);
+    root->insert(1);
+    root->insert(8);
+    root->insert(4);
+    root->insert(6);
 
-    rot->intrav();
+    std::cout << "Recursive preorder traversal:\n";
+    root->printRecursivePreorderTraversal();
     std::cout << std::endl;
 
-    rot->remove(6);
+    std::cout << "Recursive inorder traversal:\n";
+    root->printRecursiveInorderTraversal();
+    std::cout << std::endl;
 
-    rot->intrav();
+    std::cout << "Recursive postorder traversal:\n";
+    root->printRecursivePostorderTraversal();
+    std::cout << std::endl;
+
+    std::cout << "Breadth first level order traversal:\n";
+    root->printLevelOrderTraversal();
+    std::cout << std::endl;
+
+    root->remove(3);
+
+//    std::cout << "Recursive preorder traversal:\n";
+//    root->printRecursivePreorderTraversal();
+//    std::cout << std::endl;
+
+//    std::cout << "Delete 3\n";
+//    root->remove(3);
+
+//    std::cout << "Recursive preorder traversal:\n";
+//    root->printRecursivePreorderTraversal();
+//    std::cout << std::endl;
+
+//    std::cout << "Delete 4\n";
+//    root->remove(4);
+
+    std::cout << "Recursive preorder traversal:\n";
+    root->printRecursivePreorderTraversal();
+    std::cout << std::endl;
+
+    std::cout << "Recursive inorder traversal:\n";
+    root->printRecursiveInorderTraversal();
+    std::cout << std::endl;
+
+    std::cout << "Recursive postorder traversal:\n";
+    root->printRecursivePostorderTraversal();
+    std::cout << std::endl;
+
+    std::cout << "Breadth first level order traversal:\n";
+    root->printLevelOrderTraversal();
     std::cout << std::endl;
 
     return 0;
--- Oblig2/node.h
+++ Oblig2/node.h
@@ -1,51 +1,291 @@
 #ifndef NODE_H
 #define NODE_H
 
+#include <iostream>
 
+namespace ADS101 {
+
+template <typename T>
 class Node
 {
 public:
-    Node(int data) : mData{ data }, leftSub{nullptr}, rightSub{nullptr}
+    // Constructor takes a data of some type
+    Node(T data) : mData{ data }, left{nullptr}, right{nullptr}
     {
     }
 
-    void insert(int data);
+    // Inserts new node with given data
+    void insert(T data)
+    {
+        if(data < mData)
+        {
+            if(left != nullptr)
+            {
+                left->insert(data);
+            }
+            else
+            {
+                left = new Node(data);
+            }
+        }
+        else
+        {
+            if(right != nullptr)
+            {
+                right->insert(data);
+            }
+            else
+            {
+                right = new Node(data);
+            }
+        }
+    }
 
-    void intrav();
+    // Public function used to remove a node,
+    // calls the private function
+    void remove(T data)
+    {
+        if(exists(this, data))
+        {
+            std::cout << "Remvoing " << data << std::endl;
+            remove(this, data);
+        }
+        else
+        {
+            std::cout << "Did not find data to remove\n";
+        }
+    }
 
-    void remove(int data);
+    // Public function used to check if a given node exists,
+    // calls the private function
+    bool exists(T data)
+    {
+        return exists(this, data);
+    }
 
-    Node* find(int data);
+    // Replaces a given node with a node with the data given
+    void replaceNode(T data, T replacementData)
+    {
+        if(left)
+        {
+            if(left->data() == data)
+            {
+                auto replacementNode = new Node(replacementData);
+                replacementNode->left = left->left;
+                replacementNode->right = left->right;
+                left = nullptr;
+                left = replacementNode;
+            }
+            else
+            {
+                left->replaceNode(data, replacementData);
+            }
+        }
 
-    void replaceNode(int dataToReplace, int replacementData);
+        if(right)
+        {
+            if(right->data() == data)
+            {
+                auto replacementNode = new Node(replacementData);
+                replacementNode->left = right->left;
+                replacementNode->right = right->right;
+                right = nullptr;
+                right = replacementNode;
+            }
+            else
+            {
+                right->replaceNode(data, replacementData);
+            }
+        }
+    }
+
+    // Prints tree using recursive preorder traversal
+    void printRecursivePreorderTraversal()
+    {
+        std::cout << mData << " ";
+
+        if(left)
+            left->printRecursivePreorderTraversal();
+
+        if(right)
+            right->printRecursivePreorderTraversal();
+    }
+
+
+    // Prints tree using recursive inorder traversal
+    void printRecursiveInorderTraversal()
+    {
+        if(left)
+            left->printRecursiveInorderTraversal();
+
+        std::cout << mData << " ";
+
+        if(right)
+            right->printRecursiveInorderTraversal();
+    }
+
+    // Prints tree using recursive postorder traversal
+    void printRecursivePostorderTraversal()
+    {
+        if(left)
+            left->printRecursivePostorderTraversal();
+
+        if(right)
+            right->printRecursivePostorderTraversal();
+
+        std::cout << mData << " ";
+    }
+
+    // Prints tree using breadth-first level order traversal
+    void printLevelOrderTraversal()
+    {
+        int h = height();
+        for(int i = 1; i <= h; i++)
+        {
+            printLevel(i);
+        }
+    }
 
     int data() { return mData; }
 
 private:
-    void remove(Node& Comparable, Node* node);
-
-    Node* findMin(Node* node);
-    Node* findMax(Node* node);
-
-    bool operator==(const Node& node)
+    // Returns if the given data is found
+    bool exists(Node* node, T data)
     {
-        return mData == node.mData;
+        do
+        {
+            if(data > node->mData)
+            {
+                node = node->right;
+            }
+            else if(data < node->mData)
+            {
+                node = node->left;
+            }
+            else
+            {
+                return true;
+            }
+        } while(node != nullptr);
+
+        return false;
     }
 
-    bool operator<(const Node& node)
+    // Removes a node with the given data, node is the root node
+    Node* remove(Node* node, T data)
     {
-        return mData < node.mData;
+        if(node == nullptr)
+        {
+            return node;
+        }
+
+        // Go left
+        if(data < node->mData)
+        {
+            node->left = remove(node->left, data);
+        }
+        // Go right
+        else if(data > node->mData)
+        {
+            node->right = remove(node->right, data);
+        }
+        // At correct node
+        else
+        {
+            // If two nodes
+            if(node->left && node->right)
+            {
+                auto temp = findMin(node->right);
+                if(temp)
+                {
+                    node->mData = temp->mData;
+                    node->right = remove(node->right, node->mData);
+                }
+            }
+            // If no nodes
+            else if(!node->left && !node->right)
+            {
+                node = nullptr;
+            }
+            // If right node
+            else if(node->right)
+            {
+                auto temp = node->right;
+                node = nullptr;
+                return temp;
+            }
+            // If left node
+            else if(node->left)
+            {
+                auto temp = node->left;
+                node = nullptr;
+                return temp;
+            }
+        }
+        return node;
     }
 
-    bool operator>(const Node& node)
+    // Returns the lowest node from the given root node
+    Node* findMin(Node* node)
     {
-        return mData < node.mData;
+        auto currentNode = node;
+
+        while(currentNode->left != nullptr)
+        {
+            currentNode = currentNode->left;
+        }
+
+        return currentNode;
     }
 
-    int mData;
-    Node* leftSub;
-    Node* rightSub;
-    Node* parent;
+    // Returns the highest node from the given root node
+    Node* findMax(Node* node)
+    {
+        auto currentNode = node;
+
+        while(currentNode != nullptr)
+        {
+            currentNode = currentNode->right;
+        }
+
+        return currentNode;
+    }
+
+    // Returns the height of the tree
+    int height()
+    {
+        int leftHeight = left ? left->height() : 0;
+        int rightHeight = right ? right->height() : 0;
+
+        // To make sure that we get the max height
+        return std::max(leftHeight, rightHeight) + 1;
+    }
+
+    // Recursive function to print data on the level given
+    void printLevel(int level)
+    {
+        if(level == 1)
+        {
+            std::cout << mData << " ";
+        }
+        else if(level > 1)
+        {
+            if(left)
+            {
+                left->printLevel(level-1);
+            }
+            if(right)
+            {
+                right->printLevel(level-1);
+            }
+        }
+    }
+
+    T mData;
+    Node* left;
+    Node* right;
 };
+
+}
 
 #endif // NODE_H
