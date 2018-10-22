9f1143c0c0fcdceb4101c961f08cdc9414c4200f
Skau <kskau93@gmail.com>
2018-10-22 12:54:10 +0200

    Templated mergesort

--- Oblig3/Mergesort/main.cpp
+++ Oblig3/Mergesort/main.cpp
@@ -1,23 +1,39 @@
 #include <iostream>
+#include <vector>
 
-void merge_sort(int a[], int low, int high);
-void merge(int a[], int left_low, int left_high, int right_low, int right_high);
+template<typename T>
+void merge_sort(std::vector<T>& a);
+template<typename T>
+void merge_sort(std::vector<T>& a, int low, int high);
+template<typename T>
+void merge(std::vector<T>& a, int left_low, int left_high, int right_low, int right_high);
+template<typename T>
+void print(std::vector<T>& a);
 
 int main()
 {
-    const int size = 9;
-    int data[size] = {8, 3, 2, 7, 6, 1, 9, 5, 4};
+    std::vector<int> data1 = {8, 3, 2, 7, 6, 1, 9, 5, 4};
+    std::vector<char> data2 = {'h', 'r', 'n', 'a', 'z', 'y', 'w', 'k', 'l'};
 
-    merge_sort(data, 0, size-1);
+    merge_sort(data1);
 
-    for(int i = 0; i < size; ++i)
-        std::cout << data[i] << " ";
+    merge_sort(data2);
 
-    std::cout << std::endl;
+    print(data1);
+
+    print(data2);
+
     return 0;
 }
 
-void merge_sort(int a[], int low, int high)
+template<typename T>
+void merge_sort(std::vector<T>& a)
+{
+    merge_sort(a, 0, a.size() - 1);
+}
+
+template<typename T>
+void merge_sort(std::vector<T>& a, int low, int high)
 {
     if (low >= high)
     {
@@ -32,7 +48,8 @@ void merge_sort(int a[], int low, int high)
     }
 }
 
-void merge(int a[], int left_low, int left_high, int right_low, int right_high)
+template<typename T>
+void merge(std::vector<T>& a, int left_low, int left_high, int right_low, int right_high)
 {
     int length = right_high - left_low + 1;
     int temp[length];
@@ -51,4 +68,15 @@ void merge(int a[], int left_low, int left_high, int right_low, int right_high)
 
     for (int i=0; i< length; ++i)
         a[left_low++] = temp[i];
+}
+
+template<typename T>
+void print(std::vector<T> &a)
+{
+    for(auto& data : a)
+    {
+        std::cout << data << " ";
+    }
+
+    std::cout << std::endl;
 }
